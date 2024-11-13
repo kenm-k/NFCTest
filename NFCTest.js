0f3711167ed3b7930ffaf01dde76395cc1af52dc
ken <131577778+kenm-k@users.noreply.github.com>
2024-11-13 16:34:24 +0900

    first commit
    yeah

---
+++ NFCTest.js
@@ -0,0 +1,40 @@
+const scan = async () => {
+    try {
+        const reader = new NDEFReader()
+        await reader.scan()
+
+        // Scanは起動しているが、NFCタグからデータが読み込めなかった
+        reader.addEventListener('error', (event) => {
+            //console.log(error)
+            WriteLog(error);
+        })
+
+        // データを読み込んだ
+        reader.addEventListener('reading', ({ serialNumber, message }) => {
+            const record = message.records[0]
+            const { data, encoding, recordType } = record
+            // recordTypeごとにdecode処理を実行する
+            if (recordType === 'text') {
+                const textDecoder = new TextDecoder(encoding)
+                const text = textDecoder.decode(data)
+                //console.log(`Text: ${text}`)
+                WriteLog(`Text: ${text}`);
+            }
+        })
+    } catch (error) {
+        // Scan起動失敗
+        //console.error(error)
+        WriteLog(error);
+    }
+}
+
+function ScanNFC()
+{
+    scan();
+}
+
+function WriteLog(text)
+{
+    let log = document.getElementById("log");
+    log.textContent += text + "\n";
+}
