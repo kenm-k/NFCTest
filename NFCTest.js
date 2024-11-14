e6975172f81458f4df5d76c29465515e17b5492c
ken <131577778+kenm-k@users.noreply.github.com>
2024-11-15 00:29:28 +0900

    Add files via upload

--- NFCTest.js
+++ NFCTest.js
@@ -69,6 +69,8 @@ async function Matching(id) {
         tempDoc = doc;
     });
 
+    console.log(tempDoc.data());
+
     if (tempDoc.exists()) {
         WriteP(`あなたの学籍番号：${tempDoc.data()}`);
     }
